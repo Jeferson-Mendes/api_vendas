8c5c42c32ec8470820bbe1c3d8d3e26f5cd58f8b
Jeferson-Mendes <jefersonmendes3c@gmail.com>
2021-07-11 14:21:50 -0300

    feat: email with ethereal email and template handlebars

--- src/modules/users/services/SendForgotPasswordEmailService.ts
+++ src/modules/users/services/SendForgotPasswordEmailService.ts
@@ -3,6 +3,7 @@ import { getCustomRepository } from 'typeorm';
 
 import { UsersRepository } from '../typeorm/repositories/UsersRepository';
 import { UserTokensRepository } from '../typeorm/repositories/UserTokensRepository';
+import EtherealMail from '@config/mail/EtherealMail';
 
 interface IRequest {
   email: string;
@@ -21,7 +22,12 @@ class SendForgotPasswordEmailService {
 
     const { token } = await userTokenRepository.generateToken(userExists.id);
 
-    console.log(token);
+    // console.log(token);
+
+    await EtherealMail.sendMail({
+      to: email,
+      body: `Solicitação de redefinição de senha recebida. Este é o seu token: ${token}`,
+    });
   }
 }
 
